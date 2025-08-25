638f7b601bb03a96fa0d04ee3556ce49c10ff6a7
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-08-25 11:27:08 +0300

    Test if a ship can be hit, if a ship can be sunk

--- tests/main.test.js
+++ tests/main.test.js
@@ -4,3 +4,13 @@ it('Ship has a length', () => {
     const ship = new Ship(3);
     expect(ship.length).toBe(3);
 });
+
+it('Ship can be hit', () => {
+    const ship = new Ship(3, 2, false);
+    expect(ship.hitsAmount).toBe(2);
+});
+
+it('Ship can be sunk', () => {
+    const ship = new Ship(3, 3, true);
+    expect(ship.sunk).toBe(true);
+});
