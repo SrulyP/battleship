41aa83b168e9b05d36ea5f09105f0789dcade046
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-08-27 16:11:43 +0300

    Add updateGrid with its helper checkSquareData, which updates the two players grids, hiding the PC ships

--- src/scripts/gamedriver2.js
+++ src/scripts/gamedriver2.js
@@ -117,4 +117,65 @@ export const gameApp = {
             }
         }, 1500);
     },
+
+    // Put classes to display if cell is a ship, was hit, was a miss, or is untouched
+    updateGrid(player, grid) {
+        // get all squares (pc's or player's)
+        const squares = grid.querySelectorAll(`.grid-piece.${player.name}`);
+
+        for (let i = 0; i < squares.length; i++) {
+            const square = squares[i];
+            const x = Number(square.dataset.x);
+            const y = Number(square.dataset.y);
+
+            // check if the square has been hit, missed, or has not been attacked yet.
+            const tag = this.checkSquareData(player, x, y);
+
+            // reset and apply classes
+            square.classList.remove('hit', 'miss', 'ship', 'empty');
+
+            if (tag === 'hit') {
+                square.classList.add('hit');
+            } else if (tag === 'miss') {
+                square.classList.add('miss');
+            } else if (tag === 'ship') {
+                square.classList.add('ship');
+            } else {
+                square.classList.add('empty');
+            }
+        }
+    },
+
+    checkSquareData(player, x, y) {
+        const playerBoard = player.gameBoard.getBoard();
+        const squareData = playerBoard[x][y];
+        const hasShip = squareData instanceof Ship;
+
+        // Check if the square was shot at
+        let wasShot = false;
+        for (let i = 0; i < board.shots.length; i++) {
+            // take the cords of the current shot
+            const [sx, sy] = board.shots[i];
+            // check if the cords of the shot are equal to the cords of the passed square
+            if (sx === x && sy === y) {
+                wasShot = true;
+                break;
+            }
+        }
+
+        // If it was shot at, check if it was a miss
+        let isMiss = false;
+        for (let i = 0; i < board.misses.length; i++) {
+            const [mx, my] = board.misses[i];
+            if (mx === x && my === y) {
+                isMiss = true;
+                break;
+            }
+        }
+
+        if (wasShot && hasShip) return 'hit';
+        if (isMiss || (wasShot && !hasShip)) return 'miss';
+        if (hasShip && player.name !== 'computer') return 'ship';
+        return 'empty';
+    },
 };
