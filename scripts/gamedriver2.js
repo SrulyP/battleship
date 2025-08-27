e9203c74143a815a1f9df14d7b8c3e5cdbc3c675
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-08-27 11:40:38 +0300

    Start rewriting gamedriver

---
+++ scripts/gamedriver2.js
@@ -0,0 +1,10 @@
+import { Ship, Gameboard, Player } from './classes.js';
+
+function takeTurn(player) {
+    if (player.name === 'computer') {
+        computerMove();
+    }
+    // user turn
+}
+
+function computerMove() {}
